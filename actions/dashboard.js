07a2cb0c991088a789b1905b3ed80b63bd9cb6cd
Meet Trivedi <116267381+TrivediMeet@users.noreply.github.com>
2025-02-26 11:27:23 +0530

    Create Account Drawer and Functionality

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -1,16 +1,16 @@
-"user server";
+"use server"
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
-const serializTransaction = (obj) =>{
-    const serialized = {...obj};
-
-    if(obj.balance)
-    {
-        serialized.balance = obj.balance.toNumber();
-    }
-}
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+ 
+};
 
 export async function createAccount(data) {
   try {
@@ -20,7 +20,7 @@ export async function createAccount(data) {
       throw new Error("Unauthorized");
     }
 
-    const user = await db.user.findunique({
+    const user = await db.user.findUnique({
       where: { clerkUserId: userId },
     });
 
@@ -31,45 +31,45 @@ export async function createAccount(data) {
     //convert balance to float before saving
 
     const balanceFloat = parseFloat(data.balance);
-
     if (isNaN(balanceFloat)) {
       throw new Error("Invalid balance amount");
     }
 
     //check if this is the user's first account
 
-    const existiingAccounts = await db.account.findMany({
+     const existingAccounts = await db.account.findMany({
       where: { userId: user.id },
     });
 
     const shouldBeDefault =
-      existiingAccounts.length === 0 ? true : data.isDefault;
+      existingAccounts.length === 0 ? true : data.isDefault;
 
 
       // if this account should be default, unset other default accounts
-    if (shouldBeDefault) {
-      await db.account.updateMany({
-        where: { userId: user.id, isDefault: true },
-        data: { isDefault: false },
-      });
-    }
+      if (shouldBeDefault) {
+        await db.account.updateMany({
+          where: { userId: user.id, isDefault: true },
+          data: { isDefault: false },
+        });
+      }
 
-    const account = await db.account.create({
-        data:{
-            ...data,
-            balance:balanceFloat,
-            userId: user.id,
-            isDefault: shouldBeDefault,
+      const account = await db.account.create({
+        data: {
+          ...data,
+          balance: balanceFloat,
+          userId: user.id,
+          isDefault: shouldBeDefault, // Override the isDefault based on our logic
         },
-    });
+      });
 
 
-    const serializedAccount = serializTransaction(account);
+    const serializedAccount = serializeTransaction(account);
 
     revalidatePath("/dashboard");
+    return { success: true, data: serializedAccount };
 
-    return {success:true, data:serializedAccount};
 
-
-  } catch (error) {}
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
