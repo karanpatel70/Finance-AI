3338d98552bb951436f8465638e6d1eb24bc2cc0
Meet Trivedi <116267381+TrivediMeet@users.noreply.github.com>
2025-02-28 21:23:00 +0530

    Progress Bar Done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,13 +4,29 @@ import { Plus } from "lucide-react";
 import React from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import AccountCard from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import { BudgetProgress } from "./_components/budget-progress";
 async function DashboardPage() {
-  const accounts = await getUserAccounts();
+  const [accounts, transactions] = await Promise.all([
+    getUserAccounts(),
+    // getDashboardData(),
+  ]);
+
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
+  }
 
   return (
-    <div className="px-5">
+    <div className="space-y-8">
       {/* Budget Progress */}
-
+      <BudgetProgress
+        initialBudget={budgetData?.budget}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
       {/* Overview */}
 
       {/* Accounts Grid */}
