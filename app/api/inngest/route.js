645b82c1c60fc9792d2c4bba1d61d2cb183c5c0f
Meet Trivedi <116267381+TrivediMeet@users.noreply.github.com>
2025-03-01 22:16:14 +0530

    Recurring Transaction Inngest Done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,17 @@
-import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import {
+  checkBudgetAlerts,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlerts, // <-- This is where you'll always add all your functions
+    processRecurringTransaction,
+    triggerRecurringTransactions,
+    checkBudgetAlerts,
   ],
 });
